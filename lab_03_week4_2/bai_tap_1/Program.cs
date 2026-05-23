6cd31b42078376bdca96d54891da8d78ce15f61d
beymex <nguyenthanhnhan140106@gmail.com>
2026-05-23 15:18:34 +0700

    aaa

---
+++ lab_03_week4_2/bai_tap_1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace bai_tap1
+{
+    // Thêm class bọc bên ngoài phương thức static
+    public class Program
+    {
+        public static void HandleMultipleExceptions(string input, int index)
+        {
+            int[] numbers = { 1, 2, 3 };
+
+            try
+            {
+                int parsedValue = int.Parse(input);
+                Console.WriteLine($"Gia tri tai index {index} la: {numbers[index]}");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Invalid format.");
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Console.WriteLine("Index out of range.");
+            }
+        }
+        static void Main(string[] args)
+        {
+            // Ví dụ test thử hàm:
+            HandleMultipleExceptions("123", 1);
+            HandleMultipleExceptions("abc", 0);
+            HandleMultipleExceptions("123", 5);
+        }
+    }
+}
---
+++ lab_03_week4_2/bai_tap_2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace bai_tap_2;
+class Animal
+{
+    public virtual void MakeSound()
+    {
+        Console.WriteLine("Animal makes a sound.");
+    }
+}
+class Dog : Animal
+{
+    public override void MakeSound()
+    {
+        Console.WriteLine("Dog barks.");
+    }
+}
+class Cat : Animal
+{
+    public override void MakeSound()
+    {
+        Console.WriteLine("Cat meows.");
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Animal myDog = new Dog();
+        Animal myCat = new Cat();
+
+        myDog.MakeSound();
+        myCat.MakeSound();
+    }
+}
